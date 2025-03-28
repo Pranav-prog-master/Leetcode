e844d6007cfa5767c4a1a7d4f5d32ddb2656ff49
Pranav-prog-master <pranavbadgujar843@gmail.com>
2025-03-28 23:05:05 +0530

    Day_2

--- Complete_C.c
+++ Complete_C.c
@@ -62,3 +62,42 @@ int main() {
 
     return 0;
 }
+
+
+//! ****** Day2 ******
+//? Given an integer x, return true if x is a palindrome, and false otherwise.
+//? Example 1:
+// Input: x = 121 Output: true Explanation: 121 reads as 121 from left to right and from right to left. 
+//? Example 2:
+// Input: x = -121 Output: false Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome. 
+//? Example 3:
+// Input: x = 10 Output: false Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+//? Constraints:
+// -231 <= x <= 231 - 1
+
+//?
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPalindrome(int x) {
+    if (x < 0 || (x % 10 == 0 && x != 0)) {
+        return false;
+    }
+
+    int reversedHalf = 0;
+    while (x > reversedHalf) {
+        int digit = x % 10;
+        reversedHalf = reversedHalf * 10 + digit;
+        x /= 10;
+    }
+
+    return x == reversedHalf || x == reversedHalf / 10;
+}
+
+int main() {
+    printf("%d\n", isPalindrome(121));  
+    printf("%d\n", isPalindrome(-121)); 
+    printf("%d\n", isPalindrome(10));   
+
+    return 0;
+}
